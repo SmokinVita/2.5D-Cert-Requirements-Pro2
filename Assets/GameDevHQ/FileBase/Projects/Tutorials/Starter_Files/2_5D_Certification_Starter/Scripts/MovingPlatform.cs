4f0b91935e07658b610626d64cf1e38f0d4431a4
SmokinVita <69760636+SmokinVita@users.noreply.github.com>
2022-01-04 16:42:30 +0900

    Adding moving platforms!

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/MovingPlatform.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform _targetA, _targetB, _currentTarget;
+    [SerializeField]
+    private float _speed = 1f;
+
+    void Start()
+    {
+        _currentTarget = _targetA;
+    }
+
+    void FixedUpdate()
+    {
+        if(transform.position == _targetA.position)
+        {
+            _currentTarget = _targetB;
+        }
+        else if(transform.position == _targetB.position)
+        {
+            _currentTarget = _targetA;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, _currentTarget.position, _speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            other.transform.parent = transform;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            other.transform.parent = null;
+        }
+    }
+}
